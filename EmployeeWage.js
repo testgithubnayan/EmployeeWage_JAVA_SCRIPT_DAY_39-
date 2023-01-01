3263ce76a49f7669d586a58cee3220aeb0634c3a
NAYAN S. SONI <107099130+testgithubnayan@users.noreply.github.com>
2023-01-01 22:48:55 +0530

    UC5

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,33 @@
-const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const MAX_HRS_IN_MONTH = 160;
+const NO_OF_WORKING_DAYS = 20;
 
-    let empCheck =  Math.floor(Math.random() * 10) %  2;
 
-    if (empCheck == 0) {
-        console.log("Employee is Absent");
-    } else {
-        console.log("Employee is Present");
-    }
+function getWorkingHrs(empCheck) {
+switch(empCheck) {
+    case IS_PART_TIME:
+        return PART_TIME_HOURS;
+    case IS_FULL_TIME:
+        return FULL_TIME_HOURS;
+    default:
+        return 0;    
+}
+}
+
+
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHrs(empCheck);
+}
+
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+  
+console.log("Total Days are  :-- " + totalWorkingDays + "Total Hrs are :-- " + totalEmpHrs + "Emp Wage are  :-- " + empWage);
