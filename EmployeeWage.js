75343339f14bec8a16ba0d7876b9714ad6303b07
NAYAN S. SONI <107099130+testgithubnayan@users.noreply.github.com>
2023-01-01 22:45:59 +0530

    UC3

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,23 @@
-const IS_ABSENT = 0;
+onst IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
 
-    let empCheck =  Math.floor(Math.random() * 10) %  2;
 
-    if (empCheck == 0) {
-        console.log("Employee is Absent");
-    } else {
-        console.log("Employee is Present");
-    }
+function getWorkingHrs(empCheck) {
+switch(empCheck) {
+    case IS_PART_TIME:
+        return PART_TIME_HOURS;
+    case IS_FULL_TIME:
+        return FULL_TIME_HOURS;
+    default:
+        return 0;    
+}
+}
+
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHrs(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage : " + empWage);
